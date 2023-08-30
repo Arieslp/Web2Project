03f70e2a65e0924d0d7e0e2179d7705392ad43fe
lshum <118220473+Arieslp@users.noreply.github.com>
2023-08-30 00:22:53 -0500

    Update an index page

--- index.php
+++ index.php
@@ -24,10 +24,17 @@
     // Execution on the DB server is delayed until we execute().
     $statement->execute(); 
 
+    $queryCatList = "SELECT * FROM categories";    
+    $statementCatList = $db->prepare($queryCatList);    
+    $statementCatList->execute(); 
+
     // Initialize variable
     $page_title = "";        
     $page_content = "";    
-    $page_image_filename = "";            
+    $page_image_filename = "";
+    $category_id = "";
+    $category_name = "";    
+
     
     if (isset($_GET['p'])){
         $page_permalink = htmlspecialchars($_GET['p']);
@@ -42,13 +49,31 @@
             $page_title = htmlspecialchars($row['title']);
             $page_content = htmlspecialchars($row['content']);
             $page_image_filename = htmlspecialchars($row['image_filename']);            
-        } else {
+        } 
+        else {
             echo "Error: Page not found.";
             exit;
+        } 
+    }
+    else {
+            // Set default permalink as home
+            $default_permalink = 'home';
+                   
+            $query = "SELECT * FROM pages WHERE permalink = ?";
+            $statement2 = $db->prepare($query);
+            $statement2->execute([$default_permalink]);
+        
+            if ($statement2->rowCount() > 0) {
+                $row = $statement2->fetch();
+                $page_title = htmlspecialchars($row['title']);
+                $page_content = htmlspecialchars($row['content']);
+                $page_image_filename = htmlspecialchars($row['image_filename']);                    
+            } else {
+                echo "You should begin by adding a page with a permalink of 'home' using the admin page";
+                exit;          
+            }
         }
-    } 
-    
-            
+               
 ?>
 
 <!DOCTYPE html>
@@ -57,10 +82,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="./main.css">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap" rel="stylesheet">       
+    <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap" rel="stylesheet">   
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>    
     <title>Welcome to UareSpecial!</title>
 </head>
 <body>
@@ -71,13 +101,14 @@
             <div class="headerbackground">
                 <div>
                     <a href="admin.php" class="admin-link">Admin</a>
+                    <a href="login.php" class="login-link">Login</a>
                 </div>                
                 <div class="top-left">UareSpecial CMS</div>
 				<div class="bottom-right">Content Management System</div>
             </div>
 
             <div id="menubar">
-                <nav>                    
+                <nav>
                     <?php
                     // Loop through the results and generate menu items
                     if($statement->rowCount() > 0): 
@@ -93,11 +124,36 @@
                     endif ?>                          
                 </nav>
 			</div>	
+
+            
         </header>
-        <div>
+        <!-- <div>
 				<input type="text" name="search" id="search" />
 				<button type="submit">Search</button>
-			</div>     
+			</div>    -->
+            
+            <nav class="navbar bg-body-tertiary">
+                    <div class="container-fluid">
+                        <!-- Add a sort function to allow user sort the output by category_name -->
+                        <p>Navigator by Category</p>
+                        <a href="navigatecategory.php" class="navbar-brand"> All </a>
+                        <!-- Loop through the results and generate menu items -->
+                        <?php                                             
+                        if($statementCatList->rowCount() > 0): 
+                            while ($row = $statementCatList->fetch()) {
+                                $category_name = htmlspecialchars($row['category_name']);
+                                $category_id = htmlspecialchars($row['category_id']);
+                                // Generate a menu item with the fetched data
+                                echo '<a href="inavigatecategory.php?p=' . $category_id . '">' . $category_name . '</a>';                            
+                            }                    
+                        endif ?>           
+                        <br><br>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav>
 
         <!-- Main Content -->    
         <main>      
@@ -106,11 +162,11 @@
                     <h1><?php echo $page_title ?></h1></br>
                 </div>
                 <div class="content-block">
-				    <p><?php echo $page_content ?></p></br>
+				    <p><?php echo html_entity_decode($page_content) ?></p></br>
                 </div>
                 <div class="content-block">
                     <?php if ($page_image_filename != ""): ?>
-                        <img src="images/<?php echo $page_image_filename ?>" alt="image" class="image" />
+                        <img src="uploads/<?php echo $page_image_filename ?>" alt="image" class="image" />
                     <?php endif; ?>
                 </div>    
 		    </div>                         
